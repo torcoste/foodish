9e12e488e50a4aa0b771d666217eded6f5699273
Victor Kostyuk <torcoste@gmail.com>
2019-11-16 00:55:14 +0300

    simple stack navigator

--- android/app/src/main/java/com/reactnativeapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativeapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.reactnativeapp;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 
     /**
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@
  * @format
  */
 
-import {AppRegistry, YellowBox} from 'react-native';
-import App from './App';
+import {AppRegistry} from 'react-native';
 import {name as appName} from '../app.json';
+import 'react-native-gesture-handler';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-YellowBox.ignoreWarnings(['RCTBridge required dispatch_sync']);
+import Home from './screens/Home';
+import Details from './screens/Details';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppNavigator = createStackNavigator(
+  {
+    Home: Home,
+    Details: Details,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+AppRegistry.registerComponent(appName, () => createAppContainer(AppNavigator));
