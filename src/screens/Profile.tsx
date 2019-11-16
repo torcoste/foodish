2f847af353198aa992f2b80c6bbad8462ca97ea4
Victor Kostyuk <torcoste@gmail.com>
2019-11-16 07:16:06 +0300

    profile: icons and components

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -16,6 +16,7 @@ import {
 import LinearGradient from 'react-native-linear-gradient';
 import Carousel from 'react-native-snap-carousel';
 import {FlatList} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
@@ -43,11 +44,11 @@ export default class ProfileScreen extends React.Component {
         <View
           style={{
             aspectRatio: 1,
-            height: 100,
+            height: (Dimensions.get('screen').height / 100) * 12,
             backgroundColor: item.color ? item.color : 'white',
             borderRadius: 1000,
           }}></View>
-        <Text style={{marginTop: 8}}>Achievment {index}</Text>
+        <Text style={{marginTop: 8, color: 'white'}}>Achievment {index}</Text>
       </View>
     );
   }
@@ -58,7 +59,20 @@ export default class ProfileScreen extends React.Component {
         <LinearGradient
           colors={['#4c669f', '#3b5998', '#192f6a']}
           style={styles.header}>
-          <Text style={styles.usernameText}>{'Sandy'.toLocaleUpperCase()}</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingHorizontal: 18,
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+            }}>
+            <Text style={styles.usernameText}>
+              {'Sandy'.toLocaleUpperCase()}{' '}
+            </Text>
+            <TouchableOpacity>
+              <Icon name="cog" size={30} color="rgba(255, 255, 255, 0.7)" />
+            </TouchableOpacity>
+          </View>
           <View style={styles.sliderContainer}>
             <Carousel
               ref={c => (this._slider1Ref = c)}
@@ -66,22 +80,54 @@ export default class ProfileScreen extends React.Component {
               renderItem={this.sliderRenderItem}
               sliderWidth={Dimensions.get('screen').width}
               itemWidth={150}
-              firstItem={1}
-              inactiveSlideScale={0.7}
-              inactiveSlideOpacity={0.7}
+              firstItem={0}
+              inactiveSlideScale={0.8}
+              inactiveSlideOpacity={0.9}
               loop={true}
               loopClonesPerSide={2}
               autoplay={true}
               autoplayDelay={500}
-              autoplayInterval={2000}
+              autoplayInterval={2500}
               onSnapToItem={index => this.setState({slider1ActiveSlide: index})}
             />
+          </View>
+          <View
+            style={{
+              backgroundColor: 'rgba(255,255,255,0.7)',
+              padding: 8,
+              paddingHorizontal: 24,
+              borderRadius: 1000,
+              alignSelf: 'center',
+              alignContent: 'center',
+            }}>
+            <Text
+              style={{
+                alignSelf: 'center',
+                alignContent: 'center',
+                color: '#3b5998',
+              }}>
+              Ambassador
+            </Text>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+            <View style={styles.textBlockHeaderContainer}>
+              <Text style={[styles.textBlockHeader, styles.textBlockHeaderHeading]}>12 Kg</Text>
+              <Text style={styles.textBlockHeader}>waste reduced</Text>
+            </View>
+            <View style={styles.textBlockHeaderContainer}>
+            <Text style={[styles.textBlockHeader, styles.textBlockHeaderHeading]}>+3</Text>
+              <Text style={styles.textBlockHeader}>new friends</Text>
+            </View>
+            <View style={styles.textBlockHeaderContainer}>
+            <Text style={[styles.textBlockHeader, styles.textBlockHeaderHeading]}>27 eur</Text>
+              <Text style={styles.textBlockHeader}>saved</Text>
+            </View>
           </View>
         </LinearGradient>
         <FlatList
           style={styles.body}
           data={menu}
-          keyExtractor={({text}) => (text)}
+          keyExtractor={({text}) => text}
           renderItem={({item, index}) => (
             <TouchableOpacity style={styles.menuItem} key={index}>
               <Text style={{fontSize: 18}}>{item.text}</Text>
@@ -97,9 +143,23 @@ const styles = StyleSheet.create({
   header: {
     flex: 1,
     justifyContent: 'space-evenly',
+    minHeight: 100,
   },
   body: {
     flex: 1,
+  },
+  textBlockHeader: {
+    color: 'white',
+    fontSize: 12
+  },
+  textBlockHeaderHeading: {
+    fontSize: 20,
+    marginBottom: 4
+  },
+  textBlockHeaderContainer: {
+    alignItems: "center",
+    flex: 1,
+    paddingHorizontal: 8,
   },
   usernameText: {
     fontSize: 18,
@@ -107,18 +167,22 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
     color: '#ffffff',
-    marginTop: Platform.OS === 'android' ? 10 : 40,
+    flex: 1,
+    zIndex: 9,
+    position: 'absolute',
+    left: 0,
+    right: 0,
   },
   sliderContainer: {
-    paddingVertical: 30,
+    //
   },
   menuItem: {
     height: 50,
     borderBottomWidth: 1,
-    borderColor: "rgba(0, 0, 0, 0.1)",
-    justifyContent: "center",
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
     padding: 10,
-    paddingLeft: 24
+    paddingLeft: 48,
   },
 });
 
@@ -129,4 +193,9 @@ const ENTRIES1 = [
   {color: 'yellow'},
 ];
 
-const menu = [{id: 1, text: 'Menu 1'}, {id: 2, text: 'Point 2'}, {id: 3, text: 'There is 3'}, {id: 4, text: '4st item'}];
+const menu = [
+  {id: 1, text: 'Menu 1'},
+  {id: 2, text: 'Point 2'},
+  {id: 3, text: 'There is 3'},
+  {id: 4, text: '4st item'},
+];
